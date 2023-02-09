821db95d1185589d40254982a60d95c446213d0f
Andrew Lucas <113585213+Andrew-Lucas@users.noreply.github.com>
2023-02-09 19:56:23 +0300

    Styles Part 2

--- App.js
+++ App.js
@@ -1,19 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Vibration } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.main}>
+      <StatusBar />
+      <View style={styles.topContainer}>
+        <Text style={styles.city}>Arusha</Text>
+      </View>
+      <View style={styles.bottomContainer}>
+        <Text style={styles.temperature}>27</Text>
+        <Text style={styles.condition}>Sunny</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main:{
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "royalblue"
   },
-});
+  topContainer: {
+/*     backgroundColor: "aqua", */
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  city:{
+    fontSize: 35
+  },
+  bottomContainer: {
+/*     backgroundColor: "yellowgreen", */
+    flex: 2,
+    alignItems: "center"
+  },
+  temperature: {
+    fontSize: 125
+  },
+  condition: {
+    marginTop: -25,
+    fontWeight: 400,
+    fontSize: 45
+  }
+})
